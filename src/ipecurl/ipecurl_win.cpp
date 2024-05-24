6b9a16ebe4f132e1a84fc9f4f3bdb99016c8676b
Otfried Cheong <ipe@otfried.org>
2024-05-24 22:39:32 +0200

    Update all copyright notices

--- src/ipecurl/ipecurl_win.cpp
+++ src/ipecurl/ipecurl_win.cpp
@@ -4,7 +4,7 @@
 /*
 
     This file is part of the extensible drawing editor Ipe.
-    Copyright (c) 1993-2023 Otfried Cheong
+    Copyright (c) 1993-2024 Otfried Cheong
 
     Ipe is free software; you can redistribute it and/or modify it
     under the terms of the GNU General Public License as published by
