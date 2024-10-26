c42a72a6717d90b7bd0b58f34a5a4c5325782755
Otfried Cheong <ipe@otfried.org>
2024-10-27 00:36:39 +0200

    Display shortcuts in menu

--- src/ipe-web/src/popup-menu.ts
+++ src/ipe-web/src/popup-menu.ts
@@ -123,25 +123,31 @@ export class PopupMenu {
 		setTimeout(() => {
 			// if the submenu is still showing and we are not in it, close it
 			if (!this._inSubmenu && this._submenu === m) this._closeSubmenu();
-		}, 160);
+		}, 320);
 	}
 
-	private _show(x: number, y: number, pane: HTMLDivElement, m: HTMLDivElement) {
+	private _show(
+		x: number,
+		y1: number,
+		y2: number,
+		pane: HTMLDivElement,
+		m: HTMLDivElement,
+	) {
 		m.style.left = `${x}px`;
-		m.style.top = `${y}px`;
+		m.style.top = `${y1}px`;
 		pane.appendChild(m);
 		pane.style.display = "block";
 		setTimeout(() => {
 			// wait for browser to compute size of menu
 			const h = m.offsetHeight;
-			if (y + h > window.innerHeight) {
-				if (h < y) {
-					m.style.top = `${y - h}px`;
-				} else if (y < window.innerHeight / 2) {
-					m.style.maxHeight = `${window.innerHeight - y}px`;
+			if (y1 + h > window.innerHeight) {
+				if (h < y1) {
+					m.style.top = `${y2 - h}px`;
+				} else if (y1 < window.innerHeight / 2) {
+					m.style.maxHeight = `${window.innerHeight - y1}px`;
 				} else {
 					m.style.top = "0px";
-					m.style.maxHeight = `${y}px`;
+					m.style.maxHeight = `${y2}px`;
 				}
 			}
 			this._isOpen = true;
@@ -173,7 +179,7 @@ export class PopupMenu {
 			}
 			this._menu.appendChild(item);
 		}
-		this._show(x, y, this.pane, this._menu);
+		this._show(x, y, y, this.pane, this._menu);
 	}
 
 	public openMainMenu(
@@ -187,13 +193,22 @@ export class PopupMenu {
 		for (const m of items) {
 			const item = document.createElement("div");
 			if (["normal", "checkbox", "radio"].includes(m.type)) {
-				item.innerText = m.label!;
-				item.classList.add("popup-menu-item");
+				const label = document.createElement("span");
+				label.innerText = m.label!;
+				label.classList.add("main-menu-label");
+				item.appendChild(label);
+				if (m.accelerator) {
+					const s = document.createElement("span");
+					s.innerText = m.accelerator;
+					s.classList.add("main-menu-shortcut");
+					item.appendChild(s);
+				}
+				item.classList.add("main-menu-item");
 				item.addEventListener("click", () => {
 					this.close();
 					cb(m.id!);
 				});
-				if (m.checked) item.classList.add("checked");
+				if (m.checked) label.classList.add("checked");
 			} else if (m.type === "separator") {
 				item.classList.add("popup-menu-separator");
 			} else if (m.type === "submenu") {
@@ -204,7 +219,7 @@ export class PopupMenu {
 			this._menu.appendChild(item);
 		}
 		const r = button.getBoundingClientRect();
-		this._show(r.left, r.bottom, this.pane, this._menu);
+		this._show(r.left, r.bottom, r.bottom, this.pane, this._menu);
 	}
 
 	private _openMainSubmenu(
@@ -227,7 +242,7 @@ export class PopupMenu {
 			this._submenu!.appendChild(item);
 		}
 		const r = handle.getBoundingClientRect();
-		this._show(r.right, r.top, this.subpane, this._submenu!);
+		this._show(r.right, r.top, r.bottom, this.subpane, this._submenu!);
 	}
 
 	private _openPopupSubmenu(
@@ -249,6 +264,6 @@ export class PopupMenu {
 			this._submenu!.appendChild(item);
 		}
 		const r = handle.getBoundingClientRect();
-		this._show(r.right, r.top, this.subpane, this._submenu!);
+		this._show(r.right, r.top, r.bottom, this.subpane, this._submenu!);
 	}
 }
