f5d0ccc0eac4f1a6d0b06b1c43b3667fe9d7564d
Otfried Cheong <ipe@otfried.org>
2024-11-03 20:27:45 +0100

    Build dmg image in github action

--- src/ipe/dmg.py
+++ src/ipe/dmg.py
@@ -1,6 +1,10 @@
+import os.path
+
+application = defines.get("app", "build/Ipe.app")
+appname = os.path.basename(application)
 
 files = [
-    ("build/Ipe.app", "Ipe.app"),
+    (application, appname),
     ("doc/news.txt", "news.txt"),
     ("doc/gpl.txt", "gpl.txt"),
 ]
@@ -15,7 +19,7 @@ window_rect = ((100, 100), (540, 455))
 
 icon_locations = {
     "Applications": (420, 290),
-    "Ipe.app": (180, 290),
+    appname: (180, 290),
     "news.txt": (300, 80),
     "gpl.txt": (440, 80),
 }
